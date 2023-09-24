a5ed92eb5d0b3dfc2826088f57ad02c0ab59321d
Diego721 <91755944+Diego721@users.noreply.github.com>
2023-09-24 13:46:51 -0600

    Add files via upload

---
+++ C106/face_detect.py
@@ -0,0 +1,15 @@
+import cv2
+img = cv2.imread("4f.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Cargar el archivo Clasificador en cascadas
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 3)
+print(len(faces))
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y), (x+w, y+h), (255,0,0), 2)
+
+cv2.imshow("Imagen", img)
+cv2.waitKey(0)
